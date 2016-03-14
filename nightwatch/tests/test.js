dcdbfda6239c242f01b11040d04623b1fdc70f59
Gabriel Malet <gabrielmalet@gmail.com>
2016-03-14 15:24:56 +0100

    Add tests for Nighwatch

---
+++ nightwatch/tests/test.js
@@ -0,0 +1,16 @@
+require('./../server.test');
+
+module.exports = {
+  'Demo application loaded': function (client) {
+    client
+      .url('http://localhost:3000')
+      .pause('1000');
+
+    client.expect.element('body').to.be.visible;
+    client.assert.title('Test WatchNight');
+    client.setValue('input[type=text]', 'test')
+    client.click('a[id=submit]');
+    client.expect.element('#list li').to.be.visible;
+    client.end();
+  },
+};
